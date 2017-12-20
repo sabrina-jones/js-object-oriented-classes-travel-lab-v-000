878c57b733b120834ff781877fe44a375201f16c
Sabrina Jones <sabrina.jones87@gmail.com>
2017-12-20 16:13:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+class Driver{
+  constructor (name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(year){
+    let endDate = new Date(year, -1, 31);
+    let yearsExperience = endDate.getYear() - this.startDate.getYear();
+    return yearsExperience;
+  }
+}
+
+class Route{
+  constructor (beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled(){
+
+    let verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical;
+
+    let avenues = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+    function Avenue(avenueName){
+      if (avenues.includes(avenueName)){
+        return avenues.indexOf(avenueName);
+      }
+    }
+
+    let horizontalDistance = Avenue(this.endingLocation.horizontal) - Avenue(this.beginningLocation.horizontal);
+    return verticalDistance + horizontalDistance;
+  }
+
+  estimatedTime(peak){
+    if (peak){
+      return this.blocksTravelled()/2
+    } else {
+      return this.blocksTravelled()/3
+    }
+  }
+}
